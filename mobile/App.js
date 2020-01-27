a683417d5a4062aa8958663351373d784a94ac9e
Lucas Natalicio Ferreira <lucas.ferreira@novahaus.com.br>
2020-01-27 16:25:13 -0300

    Mobile

--- mobile/App.js
+++ mobile/App.js
@@ -1,25 +1,17 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
+
+
 import { StyleSheet, Text, View } from 'react-native';
+
+import Routes from './src/routes';
+import Routers from './src/routes';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello world!</Text>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    fontWeight: 'bold',
-    backgroundColor: '#ff7020',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontWeight: 'bold',
-    fontSize: 32,
-    color: '#fff',
-  }
-});
+    <>
+      <StatusBar borStyle="light-content" backgroundColor="#7D40E7"/>
+      <Routers />
+    </>
+  )
+};
