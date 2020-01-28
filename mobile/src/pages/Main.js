a9d91deb57d916c7f15305af66c5eebea1af4527
Lucas Natalicio Ferreira <lucas.ferreira@novahaus.com.br>
2020-01-28 13:41:56 -0300

    Api map

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,8 +1,68 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, {useState, useEffect } from 'react';
+import { StyleSheet, Image, View, Text } from 'react-native';
+import  MapView, { Marker, Callout }  from 'react-native-maps';
+import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
+
 
 function Main() {
-  return <View/>
+  const [currentRegion, setCurrentRegion ] = useState(null);
+
+  useEffect(()=> {
+    async function loadInitialPosition() {
+      const { granted } = await requestPermissionsAsync();
+
+      if (granted) {
+        const { coords } = await getCurrentPositionAsync({
+          enableHighAccuracy: true,
+        });
+
+        const { latitude, longitude } = coords;
+
+        setCurrentRegion({
+          latitude,
+          longitude,
+          latitudeDelta: 0.04,
+          longitudeDelta: 0.04,
+        })
+      }
+    }
+
+    loadInitialPosition();
+  }, []);
+
+  if (!currentRegion) {
+    return null;
+  }
+
+  return (
+    <MapView initialRegion={currentRegion} style={styles.map}>
+      <Marker coordinate={{latitude: -20.595221, longitude: -47.4881952}}>
+        <Image style={styles.avatar} source={{ uri: 'https://avatars3.githubusercontent.com/u/49289526?s=460&v=4'}}/>
+
+        <Callout>
+          <View style={styles.callout}>
+            <Text style={styles.devName}>Lucas</Text>
+            <Text style={styles.devBio}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborel molestias hic unde odio et sapiente</Text>
+            <Text style={styles.devTechs}>Node, React</Text>
+          </View>
+        </Callout>
+      </Marker>
+    </MapView>
+  );
 }
 
-export default Main
+const styles = StyleSheet.create({
+  map: {
+    flex: 1
+  },
+
+  avatar: {
+    width: 54,
+    height: 54,
+    borderRadius: 4,
+    borderWidth: 4,
+    borderColor: '#FFF',
+  },
+});
+
+export default Main;
